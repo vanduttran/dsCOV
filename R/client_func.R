d3df3a5827fda2bbf555c548cebbbbe503b5768e
Riccardo Giuliani <rgiulian@rserv02.vital-it.ch>
2021-05-17 10:28:48 +0200

    mirror

--- R/client_func.R
+++ R/client_func.R
@@ -6,3 +6,11 @@
 crossmatrix <- function(x,y){
 	return(crossprod(as.matrix(x),as.matrix(y)))
 }
+
+#' @title Federated ComDim
+#' @param x first block
+#' @return x
+#' @export 
+mirror <- function(x){
+	return(x)
+}
