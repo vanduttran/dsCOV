7b73f989740a02e4f31346272d0523e6c569bbf9
Riccardo Giuliani <rgiulian@rserv02.vital-it.ch>
2021-05-18 16:11:43 +0200

    merge_cov last added

--- R/client_func.R
+++ R/client_func.R
@@ -14,3 +14,23 @@ crossmatrix <- function(x,y){
 mirror <- function(x){
 	return(x)
 }
+
+#' @title Federated ComDim
+#' @param xl list of crossproducts from first block
+#' @param yl list of crossproducts from second block
+#' @param xyl list of crossproducts betweem first and second block
+#' @return x
+#' @export 
+merge_cov <- function(xl, yl, xyl){
+  
+  cx = Reduce("+" , xl)
+  cx = cx/(nrow(cx)-1)
+  
+  cy = Reduce("+" , yl)
+  cy = cy/(nrow(cy)-1)
+  
+  cxy = Reduce("+", xyl)
+  cxy = cxy/(nrow(cxy)-1)
+  
+  return(list(cx = cx, cy = cy, cxy =cxy ))
+}
