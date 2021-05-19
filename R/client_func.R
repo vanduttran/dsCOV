5c54eb314ab4ba9e71c68ac4b3279dfec1cdbe4f
Riccardo Giuliani <rgiulian@rserv02.vital-it.ch>
2021-05-19 11:43:48 +0200

    message

--- R/client_func.R
+++ R/client_func.R
@@ -1,7 +1,7 @@
 #' @title Federated ComDim
 #' @param x first block
 #' @param y second block
-#' @return x+y
+#' @return crossprod between x and y
 #' @export 
 crossmatrix <- function(x,y){
 	return(crossprod(as.matrix(x),as.matrix(y)))
@@ -24,8 +24,11 @@ mirror <- function(x){
 merge_cov <- function(lx,ly = NULL){
 #with matrices 1701 x 4, 1701 x 6 and 1290x4, 1290 x6 order of error aoround 10^-4
   
+  lx = lapply(lx, as.matrix)
+  
   if (is.null(ly)){
     
+    #lcent = lapply(lx, function(x) {scale(x, scale = F)}) #data already centered I presume
     cxs = Reduce("+", lapply(lx, crossprod))
     Cx = cxs/Reduce("+", lapply(lx, function(x){nrow(x)-1}))
     
@@ -34,6 +37,8 @@ merge_cov <- function(lx,ly = NULL){
   
   else{
     #must be generalized
+    ly = lapply(ly, as.matrix)
+    
     cxys =  Reduce("+", list(crossprod(lx[[1]], ly[[1]]), crossprod(lx[[2]], ly[[2]])))
     Cxy = cxys/Reduce("+", lapply(lx, function(x){nrow(x)-1}))
     return(Cxy)
