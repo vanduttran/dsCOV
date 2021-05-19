c6629309ca7715aefb5180565c21b77280488b0f
Riccardo Giuliani <rgiulian@rserv02.vital-it.ch>
2021-05-19 10:58:42 +0200

    merge_cov

--- R/client_func.R
+++ R/client_func.R
@@ -16,21 +16,25 @@ mirror <- function(x){
 }
 
 #' @title Federated ComDim
-#' @param xl list of crossproducts from first block
-#' @param yl list of crossproducts from second block
-#' @param xyl list of crossproducts betweem first and second block
+#' @param xl list of centered matrices from first block
+#' @param yl list of centered matrices from second block
 #' @return x
 #' @export 
-merge_cov <- function(xl, yl, xyl){
+merge_cov <- function(lx,ly = NULL){
+#with matrices 1701 x 4, 1701 x 6 and 1290x4, 1290 x6 order of error aoround 10^-4
   
-  cx = Reduce("+" , xl)
-  cx = cx/(nrow(cx)-1)
+  if (is.null(ly)){
+    
+    cxs = Reduce("+", lapply(lx, crossprod))
+    Cx = cxs/Reduce("+", lapply(lx, function(x){nrow(x)-1}))
+    
+    return(Cx)
+  }
   
-  cy = Reduce("+" , yl)
-  cy = cy/(nrow(cy)-1)
-  
-  cxy = Reduce("+", xyl)
-  cxy = cxy/(nrow(cxy)-1)
-  
-  return(list(cx = cx, cy = cy, cxy =cxy ))
+  else{
+    #must be generalized
+    cxys =  Reduce("+", list(crossprod(lx[[1]], ly[[1]]), crossprod(lx[[2]], ly[[2]])))
+    Cxy = cxys/Reduce("+", lapply(lx, function(x){nrow(x)-1}))
+    return(Cxy)
+  }
 }
